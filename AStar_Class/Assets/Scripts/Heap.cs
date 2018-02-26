e3fd66cfb32d842e00bd190c164dd3f83147e102
Unknown <ilkka.tyni@cs.tamk.fi>
2018-02-26 20:03:04 +0200

    AStarFinal

---
+++ AStar_Class/Assets/Scripts/Heap.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+ 
+public class Heap<T> where T : IHeapItem<T>
+{
+
+    T[] aItems;
+    int iCurrentItemsCount;
+
+    // Constructor...
+    public Heap(int iHeapSize)
+    {
+        // Don't want to resize the array, is expensive!
+        aItems = new T[iHeapSize];
+    }
+
+    public void Add(T item)
+    {
+        // Want item to know it's index so we create an interface:
+        // See below interface definition
+        // This will beed to be implemented by the Node
+
+        item.HeapIndex = iCurrentItemsCount;
+        aItems[iCurrentItemsCount] = item;
+        SortUp(item);
+        ++iCurrentItemsCount;
+    }
+
+
+    public T RemoveFirst()
+    {
+        T first = aItems[0];
+        --iCurrentItemsCount;
+        aItems[0] = aItems[iCurrentItemsCount];
+        aItems[0].HeapIndex = 0;
+        SortDown(aItems[0]);
+        return first;
+    }
+
+    public void UpdateItem(T item)
+    {
+        // Only ever increase priority in our use case.
+        SortUp(item);
+    }
+
+    public int Count
+    {
+        get
+        {
+            return iCurrentItemsCount;
+        }
+    }
+
+
+
+    public bool Contains(T item)
+    {
+        return Equals(aItems[item.HeapIndex], item);
+    }
+
+    void SortDown(T item)
+    {
+        while (true)
+        {
+            int iLeft = (item.HeapIndex * 2) + 1;
+            int iRight = (item.HeapIndex * 2) + 2;
+            int iSwapIndex = 0;
+            if (iLeft < iCurrentItemsCount)
+            {
+                iSwapIndex = iLeft;
+
+                if (iRight < iCurrentItemsCount)
+                {
+                    if (aItems[iLeft].CompareTo(aItems[iRight]) < 0)
+                        iSwapIndex = iRight;
+                }
+
+                if (item.CompareTo(aItems[iSwapIndex]) < 0)
+                    Swap(item, aItems[iSwapIndex]);
+                else
+                    return;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+
+
+
+    void SortUp(T item)
+    {
+        int iParentIndex = (item.HeapIndex - 1) / 2;
+        while (true)
+        {
+            T parentItem = aItems[iParentIndex];
+            if (item.CompareTo(parentItem) > 0)
+            {
+                // IF > returns 1
+                // IF == return 0
+                // IF < return -1
+                Swap(item, parentItem);
+            }
+            else
+            {
+                break;
+            }
+
+            iParentIndex = (item.HeapIndex - 1) / 2;
+        }
+    }
+
+
+    void Swap(T itemA, T itemB)
+    {
+        aItems[itemA.HeapIndex] = itemB;
+        aItems[itemB.HeapIndex] = itemA;
+        int iTemp = itemA.HeapIndex;
+        itemA.HeapIndex = itemB.HeapIndex;
+        itemB.HeapIndex = iTemp;
+    }
+}
+
+public interface IHeapItem<T> : IComparable<T>
+{
+    int HeapIndex
+    {
+        get;
+        set;
+    }
+}
---
+++ AStar_Class/Assets/Scripts/Node.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : IHeapItem<Node>{
+
+	public bool m_bIsBlocked;
+	public Vector3 m_vPosition;
+
+    public int m_iGridX;
+    public int m_iGridY;
+
+    public int m_iGCost;
+    public int m_iHCost;
+
+    public Node m_nParent;
+    public bool m_bIsPath;
+
+    int m_iHeapIndex;
+
+    public int m_ifCost
+    {
+        get { return m_iGCost + m_iHCost; }
+    }
+
+    public int HeapIndex
+    {
+        get
+        {
+            return m_iHeapIndex;
+        }
+
+        set
+        {
+            m_iHeapIndex = value;
+        }
+    }
+
+    public int CompareTo(Node node)
+    {
+        int iComp = m_ifCost.CompareTo(node.m_ifCost);
+        if (iComp == 0)
+        {
+            iComp = m_iHCost.CompareTo(node.m_iHCost);
+        }
+        return ~iComp;
+    }
+
+    public Node (bool bIsBlocked, Vector3 vPos, int x, int y, bool _bIsPath)
+	{
+		m_bIsBlocked = bIsBlocked;
+		m_vPosition = vPos;
+        m_iGridX = x;
+        m_iGridY = y;
+        m_bIsPath = _bIsPath;
+	}
+
+    
+}
